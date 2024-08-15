58931bed19055d2fa780a455c6335a025f3e05b8
Mikenifty <stuartneff@yahoo.com>
2024-08-15 09:23:37 -0400

    shopping cart functionality without supercoins

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@
 import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
+//import SuperCoin from './Components/SuperCoin';
+
 import './App.css'
+
 const App = () => {
   return (
  
@@ -10,6 +13,7 @@ const App = () => {
         <h1 className='app-heading'>E-Commerce Application</h1>
         <ProductList />
         <ShoppingCart />
+        
       </div>
 
   );
